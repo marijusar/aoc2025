1959840fb44516a96742811afd8e8fde259f9708
Marijus <marijus.arliukas@kilo.solutions>
2025-12-06 10:38:46 +0200

    06p2

--- 06/main.py
+++ 06/main.py
@@ -7,13 +7,12 @@ path = sys.argv[1]
 with open(path) as file:
     part_one = 0
     part_two = 0
-    lines = [x.rstrip() for x in file.readlines()]
+    lines = [x.rstrip('\n') for x in file.readlines()]
     grid = [[y for y in x.strip().split(" ") if y != ""] for x in lines]
     bps = [i - 1 for i, x in enumerate(lines[-1]) if x != " "]
 
     m, n = len(grid), len(grid[0])
     x, y = len(lines), len(lines[0])
-    print(lines)
 
     ops: dict[str, Callable[[int, int], int]] = {
         "*": lambda x, y: x * y,
@@ -22,9 +21,26 @@ with open(path) as file:
 
     problems: list[list[str]] = [list(reversed(x)) for x in zip(*grid)]
 
+    total = 0
+    base = 0
+    fn = ops["+"]
+
     for j in range(y):
-        for i in range(x):
-            print(lines[i][j])
+        num = ""
+        if lines[-1][j] == "*" or lines[-1][j] == "+" :
+            fn = ops[lines[-1][j]]
+            part_two += base
+            base = 1 if lines[-1][j] == "*" else 0
+
+        for i in range(x - 1):
+            if lines[i][j] != " " :
+                num += lines[i][j]
+
+        if num :
+            base = fn(base, int(num))
+
+    part_two += base
+
 
     for problem in problems:
         op = problem[0]
